63faf44cc3da90e5cec7b80308803fc26606a75a
carolune <carolina.odman@gmail.com>
2013-09-17 20:35:42 -0400

    work on examples

--- public/assets/js/examples.js
+++ public/assets/js/examples.js
@@ -6,19 +6,14 @@ function showRandomBite() {
 	// load all the data
 	var myUrl = "http://astrocast.heroku.com/bites";
 	$.getJSON(myUrl, function (bites) {
-		generateRandomBite(bites);
-		console.log('contents: ', randomBite);
+		var count = Object.keys(bites).length;
+		console.log('count: ', count);
+		// pick a random index
+		var index = Math.floor(Math.random() * (count + 1));
+		randomBite = bites[index].name;
+		console.log('randomBite: ', randomBite);
 		$('#randombite').append(randomBite);
 	});
-}
-
-function generateRandomBite(bites) {
-	var count = Object.keys(bites);
-	console.log('count: ', count);
-	// pick a random index
-	var index = Math.floor(Math.random() * (count + 1));
-	randomBite = bites[index].name;
-	console.log('randomBite: ', randomBite);
 }
 
 function makeSearchLink() {
